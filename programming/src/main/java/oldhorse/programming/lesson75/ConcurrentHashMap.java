e6127d265140f0d3ea5cdd2e4f7bbd685db47162
macrosf@ocj <macros_f@163.com>
2017-03-24 18:55:14 +0800

    init commit

---
+++ programming/src/main/java/oldhorse/programming/lesson75/ConcurrentHashMap.java
@@ -0,0 +1,16 @@
+package oldhorse.programming.lesson75;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class ConcurrentHashMap {
+	public static void main(String[] args){
+		Map<String, String> map = new ConcurrentSkipListMap<String, String>(
+				Collections.reverseOrder());
+		map.put("a", "abstract");
+		map.put("c", "call");
+		map.put("b", "basic");
+		System.out.println(map.toString());
+	}
+}
